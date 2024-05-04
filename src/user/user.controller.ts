257a41b5943af0fd39adf98c3a060c24066dfab7
GanaF4ll <ganafall9498@gmail.com>
2024-05-05 00:34:12 +0200

    createUser and updateUser controllers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,6 +6,8 @@ type User = {
   lastName: string;
   mail: string;
   role: string;
+  birthdate?: Date;
+  password?: string;
 };
 
 export const listUsers = async (): Promise<User[]> => {
@@ -24,6 +26,56 @@ export const getUserById = async (id: number): Promise<User | null> => {
   return db.users.findUnique({
     where: {
       id,
+    },
+    // select: {
+    //   id: true,
+    //   firstName: true,
+    //   lastName: true,
+    //   mail: true,
+    //   role: true,
+    // },
+  });
+};
+
+export const createUser = async (user: Omit<User, "id">): Promise<User> => {
+  const { firstName, lastName, mail, birthdate, password, role } = user;
+  return db.users.create({
+    data: {
+      firstName,
+      lastName,
+      mail,
+      birthdate: birthdate ?? "",
+      password: password || "",
+      role,
+    },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      mail: true,
+      role: true,
+      birthdate: true,
+      password: true,
+    },
+  });
+};
+
+export const updateUser = async (
+  user: Omit<User, "id">,
+  id: number
+): Promise<User> => {
+  const { firstName, lastName, mail, birthdate, password, role } = user;
+  return db.users.update({
+    where: {
+      id,
+    },
+    data: {
+      firstName,
+      lastName,
+      mail,
+      birthdate: birthdate ?? "",
+      password: password || "",
+      role,
     },
   });
 };
