d1a5a0e932526fd4fe1d0986bf5e67fb532314c6
GanaF4ll <ganafall9498@gmail.com>
2024-05-04 11:36:15 +0200

    seeding db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,3 +8,46 @@ type User = {
   password: string;
   role: string;
 };
+
+function getUsers(): Array<User> {
+  return [
+    {
+      firstName: "Seto",
+      lastName: "Kaiba",
+      birthdate: new Date("1990-01-01"),
+      mail: "setokaiba@gmail.com",
+      password: "blueeyeswhite123",
+      role: "user",
+    },
+    {
+      firstName: "Ichigo",
+      lastName: "Kurosaki",
+      birthdate: new Date("1990-01-01"),
+      mail: "kurosaki@gmail.com",
+      password: "bankai123",
+      role: "user",
+    },
+  ];
+}
+
+async function seedUsers() {
+  const users = getUsers();
+  for (const user of users) {
+    await db.users.create({
+      data: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        birthdate: user.birthdate,
+        mail: user.mail,
+        password: user.password,
+        role: user.role,
+      },
+    });
+  }
+  const author = await db.users.findFirst({
+    where: {
+      firstName: "Seto",
+    },
+  });
+}
+seedUsers();
