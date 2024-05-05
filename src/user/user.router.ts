086ce8a6e065615fb1f492593fc10bf6630fd39e
GanaF4ll <ganafall9498@gmail.com>
2024-05-05 15:44:20 +0200

    put route

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -53,3 +53,27 @@ userRouter.post(
     }
   }
 );
+
+userRouter.put(
+  "/:id",
+  body("firstName").isString(),
+  body("lastName").isString(),
+  body("birthdate").isString(),
+  body("mail").isString(),
+  body("password").isString(),
+  body("role_id").isInt(),
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    const id: number = parseInt(req.params.id, 10);
+    try {
+      const user = req.body;
+      const updatedUser = await UserController.updateUser(user, id);
+      return res.status(200).json(updatedUser);
+    } catch (error: any) {
+      return res.status(500).json(error.message);
+    }
+  }
+);
